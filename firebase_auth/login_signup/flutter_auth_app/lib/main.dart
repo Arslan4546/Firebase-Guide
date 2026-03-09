01580603886f9f87501bd8a55e0ba4794b5c999b
Arslan4546 <arslantariq4546@gmail.com>
2026-03-09 19:01:11 +0500

    add google sign start

--- firebase_auth/login_signup/flutter_auth_app/lib/main.dart
+++ firebase_auth/login_signup/flutter_auth_app/lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_auth_app/bloc/auth_bloc/auth_bloc.dart';
 import 'package:flutter_auth_app/data/repositories/auth_repo.dart';
 import 'package:flutter_auth_app/firebase_options.dart';
 import 'package:flutter_auth_app/views/auth_view/splash_view.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
